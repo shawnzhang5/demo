c4503aaeab8f8558196973615228781d717eef89
shawnzhang5 <70153292+shawnzhang5@users.noreply.github.com>
2020-10-06 02:35:20 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -10,3 +10,32 @@ $(document).ready(function(){
              $("#register_value").show();
   });
 });
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    },5000000);
+    return false;
+});
+$(document).ready(slideShow());
+
+
+function slideShow() {
+  var listOfSlideShows = document.getElementsByClassName('slideshows');
+  for (var i = 0; i < listOfSlideShows.length; i++) {
+    listOfSlideShows[i].children[0].style.display = "block";
+    listOfSlideShows[i].children[1].style.display = "none";
+  }
+  var index = false;
+  var intervals = setInterval(function() {
+    for (var i = 0; i < listOfSlideShows.length; i++) {
+      if (index == true) {
+        listOfSlideShows[i].children[0].style.display = "block";
+        listOfSlideShows[i].children[1].style.display = "none";
+      } else {
+        listOfSlideShows[i].children[0].style.display = "none";
+        listOfSlideShows[i].children[1].style.display = "block";
+      }
+    }
+    index = !index;
+  }, 2000);
+}
