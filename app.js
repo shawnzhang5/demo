e96f1eb250339413c405de7db297e1b37cb92be2
shawnzhang5 <70153292+shawnzhang5@users.noreply.github.com>
2020-10-13 11:36:15 +0800

    Update app.js
    iconJump(icon) {}

--- app.js
+++ app.js
@@ -39,3 +39,34 @@ function slideShow() {
     index = !index;
   }, 2000);
 }
+
+function iconJump(icon) {
+  var maxJump = 6;
+  var speed = 2;
+  var jumpPos = 0;
+
+  var mode = -1;
+  if (mode == -1) {
+    mode = 0;
+    var intervals = setInterval(function() {
+      switch (mode) {
+        case 0:
+          jumpPos = jumpPos + 1 * speed;
+          icon.style.top = -jumpPos + "px";
+          if (jumpPos >= maxJump)
+            mode = 1;
+          break;
+        case 1:
+          jumpPos = jumpPos - 1 * speed;
+          icon.style.top = -jumpPos + "px";
+          if (jumpPos <= 0) {
+            mode = 2;
+            icon.style.top = 0 + "px";
+          }
+          break;
+        case 2:
+          clearInterval(intervals);
+      }
+    }, 20);
+  }
+}
